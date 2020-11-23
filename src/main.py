474c84d2a57bf3a944b111738b20be500c3ecdd3
alexshelto <alexandershelton@icloud.com>
2020-11-22 19:35:25 -0500

    working on verifying stdout

--- src/main.py
+++ src/main.py
@@ -11,8 +11,16 @@ if __name__ == '__main__':
     print("Failed to pass build.txt & verify.txt to program")
     exit(1)
 
-  tester = Test()          #creating Test object
-  tester.build(str(sys.argv[1]))
+  build_file = sys.argv[1]
+  verify_file = sys.argv[2]
+
+  # 
+  # run tests on file before proceding to build and verify
+  #
+
+  tester = Test(build_file, verify_file)                 #creating Test object
+  tester.build()  #building with input file build
+  tester.verify()
 
 
 
--- src/test.py
+++ src/test.py
@@ -9,27 +9,40 @@ import os
 
 
 class Test:
-  def __init__(self):
+  def __init__(self, build_file: str, verify_file: str):
+    self.build_file = build_file
+    self.verify_file = verify_file
     self.executable_name = None
+    self.file_type = None
 
   '''
   build(): responsible for executing all commands inside of build.txt file
   '''
-  def build(self, file_name: str) -> None:
+  def build(self) -> None:
     try:
-      file = open(file_name, 'r')
+      file = open(self.build_file, 'r')
     except:
       print("trouble opening build file")
       exit(1)
-    #executing each command in build.txt file
+    
+    self.file_type = file.readline()  #grabbing file type to know how to build
+
+    #executing command from file
     for command in file:
+      print('>> {}'.format(command))
       os.system(command)
 
 
 
 
-  def execute(self):
+  def verify(self):
+    try:
+      file = openn(self.verify_file, 'r')
+    except:
+      print("trouble opening verify file")
+      exit(1)
     pass
+    
 
   def ls(self):
     os.system('ls -l')
