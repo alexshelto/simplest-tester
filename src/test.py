4a8de704a4fe9fc1bbc1177f0d5fb11349040283
alexshelto <alexandershelton@icloud.com>
2020-11-23 18:11:20 -0500

    working on new verify parse

--- src/test.py
+++ src/test.py
@@ -7,6 +7,20 @@ import os
 import subprocess
 from colorama import Fore, Back, Style
 
+def parseExpected(file) -> str:
+  print("Inside of parse expected")
+  constructed_str = ''
+  while True:
+    line = file.readline()
+    print("In line: " + str(line))
+    if line.replace(' ', '') == '</output>' : #or line.replace(' ', '') == '':
+      print("EXIT ")
+      break
+    else:
+      constructed_str += line
+
+  print("LEAVING")
+  return constructed_str
 
 class Test:
   def __init__(self, build_file: str, verify_file: str):
@@ -35,6 +49,21 @@ class Test:
     file.close() #closing file stream
 
 
+  def read(self):
+    try:
+      with open(self.verify_file, 'r') as myFile:
+        text = myFile.read()
+    except:
+      print("trouble opening verify file")
+      exit(1)
+
+    seperator = '@case'             #string to split verify.txt into array indexes of each cae
+    tests = text.split(seperator)  #split into list by '@case'
+
+    #loop through list to build each test case info
+    for test in tests:
+      print(test.split('FILE:'))
+
 
 
 
@@ -50,8 +79,7 @@ class Test:
     while True:
       test_name = file.readline().partition("NAME:")[2].strip()
       file_name = file.readline().partition("FILE:")[2].strip()
-      expected_output = file.readline().partition("VERIFY:")[2].replace('"', '').strip()
-      file.readline() #ignore newline
+      expected_output = parseExpected(file)
 
       if not test_name or not file_name or not expected_output:
         file.close()
@@ -61,7 +89,7 @@ class Test:
       output = ''
       exe = subprocess.Popen(['./a.out ../test-cases/'+str(file_name)],stdout=subprocess.PIPE, shell=True)
       for line in exe.stdout:
-        output += line.decode('utf-8').replace('\n', '\\n')
+        output += line.decode('utf-8')
       output = output[:-2] #removes final new line
 
       self.display_test(test_name, file_name, expected_output, output)
@@ -84,7 +112,7 @@ class Test:
     print('Input File: {}\n'.format(file))
     print('Expecting: {}'.format(expected))
     print('Output: {}'.format(output))
-    print('=' * 50 + '\n')
+    print('=' * 50 + '\n') 
 
 
   def summary(self):
